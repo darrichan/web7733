01696bd4d17b6d3ba0f927246247238db07ab0c6
Darrichan <15157418180@163.com>
2021-02-18 15:58:11 +0800

    '1'

--- 推送博客cmd指令.bat
+++ 推送博客cmd指令.bat
@@ -1,4 +1,3 @@
-npm install&&hexo cl&&hexo g&&git clone "https://github.com/darrichan/darrichan.github.io" deploy_git&&xcopy .\deploy_git\.git .\public\.git /i/s&&copy ..\CNAME\CNAME .\public&&cd .\public&&git add .&&git commit -m'1'&&git push --force --quiet "https://dc8dd9e1f2995e39b5ad59ac9897e515bca4cc47@github.com/darrichan/darrichan.github.io.git" main:main&&cd ..&&git add . &&git commit -m'1'&& git push --force --quiet "https://dc8dd9e1f2995e39b5ad59ac9897e515bca4cc47@github.com/darrichan/web7733.git" main:main&&pause
+npm install&&hexo cl&&hexo g&&rd /s/q deploy_git&&git clone "https://github.com/darrichan/darrichan.github.io" deploy_git&&xcopy .\deploy_git\.git .\public\.git /i/s&&copy ..\CNAME\CNAME .\public&&cd .\public&&git add .&&git commit -m'1'&&git push --force --quiet "https://dc8dd9e1f2995e39b5ad59ac9897e515bca4cc47@github.com/darrichan/darrichan.github.io.git" main:main&&cd ..&&git add . &&git commit -m'1'&& git push --force --quiet "https://dc8dd9e1f2995e39b5ad59ac9897e515bca4cc47@github.com/darrichan/web7733.git" main:main&&pause
 
 
-@REM rd /s/q deploy_git&&
