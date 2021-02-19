acb0f4edac2fcfa179f76274b51765e5c266846b
Darrichan <15157418180@163.com>
2021-02-19 15:48:46 +0800

    '1'

--- 推送博客cmd.bat
+++ 推送博客cmd.bat
@@ -7,5 +7,5 @@ call xcopy .\deploy_git\.git .\public\.git /i/s
 call copy .\CNAME\CNAME .\public&&cd .\public
 call git add .&&git commit -m'1'&&git push --force --quiet "https://dc8dd9e1f2995e39b5ad59ac9897e515bca4cc47@github.com/darrichan/darrichan.github.io.git" main:main
 call cd ..&& rd /s/q deploy_git
-call rd /s/q deploy_git
+call hexo cl
 call git add . &&git commit -m'1'&& git push --force --quiet "https://dc8dd9e1f2995e39b5ad59ac9897e515bca4cc47@github.com/darrichan/web7733.git" main:main
