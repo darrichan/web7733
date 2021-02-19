caa97c4bb050e0f5926aa8c8c2cd5049af6c4260
Darrichan <15157418180@163.com>
2021-02-19 10:31:03 +0800

    '1'

--- 推送博客cmd.bat
+++ 推送博客cmd.bat
@@ -2,7 +2,7 @@ call git clone https://github.com/darrichan/CNAME.git
 call hexo cl
 call hexo g
 call rd /s/q deploy_git
-call git clone "https://github.com/darrichan/darrichan.github.io" deploy_git
+call git clone "https://gitee.com/darrichan/darrichan.github.io.git" deploy_git
 call xcopy .\deploy_git\.git .\public\.git /i/s
 call copy .\CNAME\CNAME .\public&&cd .\public
 call git add .&&git commit -m'1'&&git push --force --quiet "https://dc8dd9e1f2995e39b5ad59ac9897e515bca4cc47@github.com/darrichan/darrichan.github.io.git" main:main
--- 本地显示.bat
+++ 本地显示.bat
@@ -1,1 +1,2 @@
+call cnpm install
 call hexo cl&&hexo g&& hexo s&&pause
